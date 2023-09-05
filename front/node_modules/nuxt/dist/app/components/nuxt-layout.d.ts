9e1f4e35c627c4e4d82d9ab8bdd6b2c607b5ccde
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-09-05 17:45:21 -0300

    changing things

---
+++ front/node_modules/nuxt/dist/app/components/nuxt-layout.d.ts
@@ -0,0 +1,6 @@
+import type { DefineComponent, MaybeRef } from 'vue';
+import type { PageMeta } from '#app';
+declare const _default: DefineComponent<{
+    name?: (unknown extends PageMeta['layout'] ? MaybeRef<string | false> : PageMeta['layout']) | undefined;
+}>;
+export default _default;
